cb79332d2450257cc7e753f5173619322710ab7f
ochieng2peter <ochieng2.peter@gmail.com>
2023-02-16 14:56:25 -0800

    4-puts.c👣

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main is the entry point - beggining of the program
+ *
+ * return 0 means success and ends
+ *
+ * use of puts instead of the simple printf
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
