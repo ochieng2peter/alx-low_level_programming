f979a9b64ffbf5ae8e531210018366f3e7b12ed1
ochieng2peter <ochieng2.peter@gmail.com>
2023-03-07 04:22:37 +0300

    5-strstr.c👣

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *i = "EEllen of Troy";
+	char *j = "Ellen";
+	char *k;
+
+	k = _strstr(s, j);
+	printf("%s\n", k);
+	return (0);
+}
