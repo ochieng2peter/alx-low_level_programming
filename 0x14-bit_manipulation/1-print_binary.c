35945c4a6abdaa8d2c36ca87ee3a69b1688cf212
ochieng2peter <ochieng2.peter@gmail.com>
2023-04-07 01:58:06 +0300

    Minor edits to 1-print_binary.c file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,6 @@
 void print_binary(unsigned long int n)
 {
 	int flg = 0; /* Flg to indicate if the leading zeros have been printed */
-
 	/* Initialize mask to the leftmost bit */
 	unsigned long int msk = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
@@ -16,6 +15,7 @@ void print_binary(unsigned long int n)
 	if (n == 0)
 	{
 		_putchar('0');
+		_putchar('\n');
 		return;
 	}
 
@@ -25,17 +25,19 @@ void print_binary(unsigned long int n)
 		msk >>= 1;
 	}
 
-	/* Print the binary representation of n using the msk */
+	/* Print the binary representation of n using the mask */
 	while (msk > 0)
 	{
 		if (n & msk)
 		{
 			_putchar('1');
+			_putchar('\n');
 			flg = 1; /* Set flag to indicate leading zeros have been printed */
 		}
 		else if (flg)
 		{
 			_putchar('0'); /* Print leading zeros if flag is set */
+			_putchar('\n');
 		}
 
 		msk >>= 1;
