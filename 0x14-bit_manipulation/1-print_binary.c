4128e28b4bf5ef4b13414009434c99bc24c4bce5
ochieng2peter <ochieng2.peter@gmail.com>
2023-04-10 12:10:34 +0300

    1-print_binary.c👣

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,45 +1,22 @@
 #include "main.h"
 
 /**
- * print_binary - Print binary representation of a number.
+ * print_binary - prints the binary representation
+ *		of a number(any number).
+ * @n: unsigned long integer.
  *
- * @n: Unsigned long integer to be printed in binary form.
+ * Return: nothing.
  */
 void print_binary(unsigned long int n)
 {
-	int flg = 0; /* Flg to indicate if the leading zeros have been printed */
-	/* Initialize mask to the leftmost bit */
-	unsigned long int msk = 1UL << (sizeof(unsigned long int) * 8 - 1);
-
-	/* If n is 0, simply print 0 in binary */
-	if (n == 0)
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
 	{
 		_putchar('0');
-		_putchar('\n');
-		return;
-	}
-
-	/* Find the highest bit that is set in n */
-	while ((msk & n) == 0)
-	{
-		msk >>= 1;
-	}
-
-	/* Print the binary representation of n using the mask */
-	while (msk > 0)
-	{
-		if (n & msk)
-		{
-			_putchar('1');
-			_putchar('\n');
-			flg = 1; /* Set flag to indicate leading zeros have been printed */
-		}
-		else if (flg)
-		{
-			_putchar('0'); /* Print leading zeros if flag is set */
-			_putchar('\n');
-		}
-
-		msk >>= 1;
 	}
 }
